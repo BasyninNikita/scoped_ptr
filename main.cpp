f615265edc6fce068b9078661a5b13c32e85585a
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-04-22 14:31:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,28 @@ class scoped_ptr{
 private:
     T * ptr;
 public:
-    scoped_ptr () {
+    	scoped_ptr () {
 		ptr = nullptr;
 	}
 	~scoped_ptr () {
 		delete ptr;
+	}
+	T* operator->() const { 
+		return ptr; 
+	}
+	T& operator* ()const { 
+		return *ptr; 
+	}
+	void swap(scoped_ptr&other){
+		if(ptr || other.ptr)
+			std::swap(ptr,other.ptr);
+	}
+	scoped_ptr(T * ptr_){
+		ptr=ptr_;	
+	}
+	T * reset(T * oth){
+		delete ptr;
+		ptr=oth;
 	}
 };
     
